4bc8e7feead14c0e780c751ea712ebbab49012c9
Sri Harsha Chennavajjala <meetsriharsha@gmail.com>
2016-03-20 23:36:19 -0500

    Adding Assignment 9 updates

---
+++ Source_Code/Assignment9/IonicGeolocation/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,72 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "id": "cordova-plugin-network-information.network",
+        "pluginId": "cordova-plugin-network-information",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+        "id": "cordova-plugin-network-information.Connection",
+        "pluginId": "cordova-plugin-network-information",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-directions/www/directions.js",
+        "id": "cordova-plugin-directions.Directions",
+        "pluginId": "cordova-plugin-directions",
+        "clobbers": [
+            "directions"
+        ]
+    },
+    {
+        "file": "plugins/plugin.google.maps/www/googlemaps-cdv-plugin.js",
+        "id": "plugin.google.maps.phonegap-googlemaps-plugin",
+        "pluginId": "plugin.google.maps",
+        "clobbers": [
+            "plugin.google.maps"
+        ]
+    },
+    {
+        "file": "plugins/ionic-plugin-keyboard/www/android/keyboard.js",
+        "id": "ionic-plugin-keyboard.keyboard",
+        "pluginId": "ionic-plugin-keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-network-information": "1.0.2-dev",
+    "cordova-plugin-geolocation": "1.0.1",
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-directions": "0.4.3",
+    "plugin.google.maps": "1.3.9",
+    "ionic-plugin-keyboard": "2.0.1",
+    "cordova-plugin-statusbar": "2.1.2"
+}
+// BOTTOM OF METADATA
+});
